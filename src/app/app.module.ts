a308b54426d6d29b289cf0cf206677e99abcb3ff
Cody Walker <themightycoder@gmail.com>
2019-08-13 18:54:39 -0700

    start of titlecase component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleCaseComponent } from './title-case/title-case.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleCaseComponent
   ],
   imports: [
     BrowserModule,
