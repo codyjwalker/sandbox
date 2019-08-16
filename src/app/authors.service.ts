ee6b3314fbffe6b3a8485a68f734d1ab31149b13
Cody Walker <themightycoder@gmail.com>
2019-08-16 08:30:46 -0700

    added some authors

--- src/app/authors.service.ts
+++ src/app/authors.service.ts
@@ -6,6 +6,6 @@ import { Injectable } from '@angular/core';
 export class AuthorsService {
 
   getAuthors() {
-    return ["author1", "author2", "author3"];
+    return ["author1", "author2", "author3", "authorA", "authorB", "authorC"];
   }
 }
