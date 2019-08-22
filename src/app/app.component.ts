07e98b96c77e2cfdefd7b3fdb8315e6a62ade047
Cody Walker <themightycoder@gmail.com>
2019-08-21 17:27:35 -0700

    ngSwitch statements implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,27 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
 export class AppComponent {
   title = 'sandbox';
 
-  courses = [1, 2];
+  /* For ngSwitchCase */
+  viewMode='something';
 
+  /* For *ngIf and [hidden]. */
+  courses = [1, 2];
+  courses2 = [];
+
+  /* For Favorite component */
   post = {
     title: "Title",
     isFavorite: true
   }
 
+  /* For Like component */
   tweet = {
     body: 'This right here is better than a damn tweet...',
     isLiked: false,
     numLikes: 68
   }
 
+  /* For Favorite component */
   onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
     console.log("FAVORITE CHANGED", eventArgs);
   }
