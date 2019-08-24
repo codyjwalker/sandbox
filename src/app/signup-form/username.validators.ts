4d02ed4f1e91029f6b3672786c9835d1423acb95
Cody Walker <themightycoder@gmail.com>
2019-08-24 00:34:06 -0700

    Asynchronous Operations implemented, and loader image added.

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -5,7 +5,20 @@ export class UsernameValidators {
   static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
     if ((control.value as string).indexOf(' ') >= 0)
       return { cannotContainSpace: true };
-
     return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl)
+      : Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('tight =D');
+        if (control.value == 'mosh')
+          resolve({ shouldBeUnique: true });
+        else if (control.value == 'choney')
+          resolve({ shouldBeUnique: true });
+        else resolve(null);
+      }, 2000);
+    });
   }
 }
