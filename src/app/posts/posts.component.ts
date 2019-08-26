9367e27b82b691912e524cd9735af46e1fe2f71b
Cody Walker <themightycoder@gmail.com>
2019-08-25 20:07:56 -0700

    Creating data partially implemented, HOWEVER need to figure out how http responses work after the http api deprication

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+/*
+ * TODO:
+ * Figure out more about (regarding http):
+ *  - What was depricated
+ *  - What has changed
+ *    - How it has changed
+ *  - What has remained the same
+ */
+
 
 @Component({
   selector: 'app-posts',
@@ -10,13 +19,24 @@ import { HttpClient } from '@angular/common/http';
 export class PostsComponent {
 
   posts: Object;
+  private url = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: HttpClient) {
+  constructor(private http: HttpClient) {
 
-    http.get('http://jsonplaceholder.typicode.com/posts')
+    http.get(this.url)
       .subscribe(response => {
         this.posts = response;
       });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response['id'];
+        console.log(post);
+    });
   }
 
 }
